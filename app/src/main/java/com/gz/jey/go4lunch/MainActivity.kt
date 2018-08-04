79066f6ebcc255e7ebae7c9b34c0d05fea7f5efa
JeyDef <defillon.jeremie@gmail.com>
2018-08-04 22:37:54 +0200

    Added Authentication google and facebook with Firebase

--- app/src/main/java/com/gz/jey/go4lunch/MainActivity.kt
+++ app/src/main/java/com/gz/jey/go4lunch/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.gz.jey.go4lunch
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN : Int = 123
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signInActivity()
+    }
+
+
+    private fun signInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(AuthUI.IdpConfig.GoogleBuilder().build(),
+                                              AuthUI.IdpConfig.FacebookBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.big_logo)
+                        .build(),
+                RC_SIGN_IN)
     }
 
     fun getAccountName() : String{
         return "JeyTest"
     }
-
 }
